d671f3bdff74a2577f0ea44505b0e8957b67cc8c
Nate Wilcox <nathan+dev@electriccoin.co>
2022-11-05 10:53:59 -0700

    [no-verify] TDD: Add a test for a test fn with a `Result` return type.

--- support/src/procmacro/tests.rs
+++ support/src/procmacro/tests.rs
@@ -35,3 +35,40 @@ fn unit_return() {
 
     assert_eq!(output.to_string(), expected.to_string());
 }
+
+#[test]
+fn result_return() {
+    let input = quote! {
+        fn my_test(testdir: PathBuf) -> std::io::Result<()> {
+            assert!(testdir.is_dir());
+            Ok(())
+        }
+    };
+
+    let output = transform_test_with_dir(input);
+
+    let expected = quote! {
+        #[test]
+        fn my_test() -> std::io::Result<()> {
+            let testdir =
+            ::target_test_dir_support::get_base_test_dir()
+                .join(format!("{}-{}", module_path!().replace("::", "-"), "my_test"));
+
+            match std::fs::create_dir(&testdir) {
+                Ok(()) => {}
+                Err(e) => {
+                    panic!("Could not create test dir {:?}: {}", testdir.display(), e);
+                }
+            }
+
+            my_test_impl(testdir)
+        }
+
+        fn my_test_impl(testdir: PathBuf) -> std::io::Result<()> {
+            assert!(testdir.is_dir());
+            Ok(())
+        }
+    };
+
+    assert_eq!(output.to_string(), expected.to_string());
+}
