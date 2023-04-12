aeb60570891919b65e69b82d094c7308659b7f44
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-12 11:41:13 -0700

    Swapping the order of `#[test]` annotation appears to work.

--- target-test-dir/tests/basic.rs
+++ target-test-dir/tests/basic.rs
@@ -1,15 +1,15 @@
 use target_test_dir::with_test_dir;
 
-#[with_test_dir()]
 #[test]
+#[with_test_dir()]
 fn ensure_test_dir_is_dir() {
     let testdir = get_test_dir!();
     dbg!(&testdir);
     assert!(testdir.is_dir());
 }
 
-#[with_test_dir]
 #[test]
+#[with_test_dir]
 fn ensure_test_dir_exists() {
     let testdir = get_test_dir!();
     dbg!(&testdir);
