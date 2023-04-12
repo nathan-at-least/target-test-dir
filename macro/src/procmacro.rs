a0c232d956ecadb32b4f3315873b12bb3c859454
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-12 11:28:44 -0700

    Implement expansion tests; fix integration tests to match.

--- macro/src/procmacro.rs
+++ macro/src/procmacro.rs
@@ -20,82 +20,33 @@ where
 
 fn transform_with_test_dir_inner(input: TokenStream) -> Result<TokenStream, syn::parse::Error> {
     use quote::quote;
-    use syn::{parse2, Ident, ItemFn};
+    use syn::{parse2, ItemFn};
 
-    let mut implfn: ItemFn = parse2(input)?;
+    let ItemFn {
+        attrs,
+        vis,
+        sig,
+        mut block,
+    }: ItemFn = parse2(input)?;
 
-    // Save the textual name for a generated wrapper function so that the actual #[test] has the
-    // user-specified name:
-    let testname = implfn.sig.ident;
-    let testnamestr = testname.to_string();
-
-    // Rename the user-provided implementation function to be wrapped:
-    let implname = Ident::new(&format!("{}_impl", &testnamestr), testname.span());
-    implfn.sig.ident = implname.clone();
-
-    // We want to propagate all args except the last, which is the testdir:
-    let mut wrapper_inputs = implfn.sig.inputs.clone();
-    wrapper_inputs.pop(); // Remove the last arg from propagation.
-
-    // Now we want to propagate the arg bindings in the call, with comma termination:
-    let arg_call_names = {
-        use syn::{
-            punctuated::Punctuated, spanned::Spanned, token::Comma, Error, Expr, FnArg::Typed, Pat,
-            PatIdent, PatType,
-        };
-
-        let mut pct = Punctuated::<Expr, Comma>::new();
-        for fnarg in &wrapper_inputs {
-            if let Typed(PatType { pat, .. }) = fnarg {
-                if let Pat::Ident(PatIdent { ident, .. }) = &**pat {
-                    pct.push(syn::parse2(quote! { #ident })?);
-                } else {
-                    return Err(Error::new(
-                        fnarg.span(),
-                        "unexpected pattern; expecting identifier",
-                    ));
+    // Insert the `get_test_dir` macro:
+    block.stmts.insert(
+        0,
+        syn::parse2::<syn::Stmt>(quote! {
+            macro_rules! get_test_dir {
+                () => {
+                    ::target_test_dir::get_test_dir(module_path!(), function_name!())
                 }
-            } else {
-                return Err(Error::new(
-                    fnarg.span(),
-                    "unexpected received; expecting `<identifier>: <type>`",
-                ));
             }
-        }
-
-        if !pct.is_empty() {
-            pct.push_punct(Comma::default());
-        }
-
-        pct
-    };
-
-    // Propagate the return type:
-    let output = implfn.sig.output.clone();
+        })?,
+    );
 
     Ok(quote! {
-        #[test]
-        fn #testname( #wrapper_inputs ) #output {
-            #implname (
-                #arg_call_names
-
-                // We initialize and pass the testdir in a local scope to avoid collutions in
-                // #testnate scope:
-                {
-                    let testdir =
-                    ::target_test_dir::get_base_test_dir()
-                        .join(format!("{}-{}", module_path!().replace("::", "-"), #testnamestr));
-
-                    if let Some(e) = std::fs::create_dir(&testdir).err() {
-                        panic!("Could not create test dir {:?}: {}", testdir.display(), e);
-                    };
-
-                    testdir
-                }
-            )
-        }
-
-        #implfn
+        #[::target_test_dir::named]
+        #( #attrs )*
+        #vis
+        #sig
+        #block
     })
 }
 
--- target-test-dir/tests/basic.rs
+++ target-test-dir/tests/basic.rs
@@ -1,14 +1,17 @@
-use std::path::PathBuf;
 use target_test_dir::with_test_dir;
 
 #[with_test_dir()]
-fn ensure_test_dir_is_dir(testdir: PathBuf) {
+#[test]
+fn ensure_test_dir_is_dir() {
+    let testdir = get_test_dir!();
     dbg!(&testdir);
     assert!(testdir.is_dir());
 }
 
 #[with_test_dir]
-fn ensure_test_dir_exists(testdir: PathBuf) {
+#[test]
+fn ensure_test_dir_exists() {
+    let testdir = get_test_dir!();
     dbg!(&testdir);
     assert!(testdir.exists());
 }
